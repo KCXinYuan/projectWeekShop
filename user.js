bc3e3755233b5bf3c383cec868620b8ea0e463ff
Tim Nguyen <tim.thien@yahoo.com>
2015-12-17 20:22:07 -0800

    add username printout

--- user.js
+++ user.js
@@ -2,6 +2,7 @@ var username = '';
 var phist;
 var $main = $('main');
 
+
 username = sessionStorage.userName;
 console.log(username);
 //var getHist = function(){
@@ -11,17 +12,17 @@ console.log(username);
 		data.forEach(function(order){
 			if(order.userID === username){
 				newHist($main,order.date,order.orderID);
-				
+
 				$currTable = $('#'+order.orderID);
 				$currBody = $('<tbody/>');
-				
+
 				order.purchased.forEach(function(item){
 					var $currRow = $('<tr/>');
 					$currRow.append('<td>' +item.name+ '</td>');
 					$currRow.append('<td>' +item.qty+ '</td>');
 					$currRow.append('<td>' +item.unitPrice+ '</td>');
 					$currRow.append('<td>' +(item.unitPrice*item.qty)+ '</td>');
-					
+
 					$currBody.append($currRow);
 				});
 				$currTable.prepend($currBody);
@@ -35,22 +36,23 @@ function newHist(elem,date,orderID){
 	for(var i=0;i<4;i++){
 		dispDate += dateArr[i] + ' ';
 	}
-		
-	
-	
+
+
+
 	var $headRow = $('<tr/>')
 	$headRow.append('<td>Item</td>');
 	$headRow.append('<td>Qty</td>');
 	$headRow.append('<td>Unit Price</td>');
 	$headRow.append('<td>Total Price</td>');
-	
+
 	var $header = $('<thead/>');
 	$header.append($headRow);
-	
+
 	var $table = $('<table/>',{
 		id:orderID,
 		html:$header
 	});
 	elem.prepend($table);
 	elem.prepend('<h2>' +dispDate+ '</h2><br>');
+	$main.prepend(sessionStorage.getItem('userName') + ', your purchased history.');
 }
